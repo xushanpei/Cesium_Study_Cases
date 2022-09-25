351689978cf3cac46a79e5ea1eed71c949c7066f
xushanpei <xu_shan_pei@163.com>
2022-09-25 22:40:34 +0800

    ✨ 初始化 Viewer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Init from '../views/init.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Init
     }
   ]
 })
---
+++ src/views/init.vue
@@ -0,0 +1,31 @@
+import cesium from 'vite-plugin-cesium';
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+import TOKEN from "../utils/Token";
+
+// Token 设置
+Cesium.Ion.defaultAccessToken = TOKEN;
+
+// Viewer
+let viewer = null;
+
+onMounted(() => {
+  initViewer();
+});
+
+const initViewer = () => {
+  viewer = new Cesium.Viewer("cesiumContainer");
+};
+</script>
+
+<style scoped>
+  #cesiumContainer{
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
